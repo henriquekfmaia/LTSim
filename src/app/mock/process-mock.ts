19580b9a9bf43043d451fb41463691868b97f677
Henrique Maia <henrique.maia@poli.ufrj.br>
2018-02-23 11:57:39 -0300

    imagens diferentes para o mock

--- src/app/mock/process-mock.ts
+++ src/app/mock/process-mock.ts
@@ -3,19 +3,19 @@ import { ProcessImage } from "../classes/process-image";
 
 
 export const PROCESSES_T1: Process[] = [
-    new Process(1, 'P1', 1, './assets/azul.png'),
-    new Process(2, 'P2', 1, './assets/azul.png'),
-    new Process(3, 'P3', 1, './assets/azul.png'),
+    new Process(1, 'Amarelo', 1, './assets/amarelo.png'),
+    new Process(2, 'Azul', 1, './assets/azul.png'),
+    new Process(3, 'Ouro', 1, './assets/gold.png'),
 ];
 
 export const PROCESSES_T2: Process[] = [
-    new Process(4, 'Coisa', 2, './assets/azul.png'),
-    new Process(5, 'Teste', 2, './assets/azul.png'),
+    new Process(4, 'Laranja', 2, './assets/laranja.png'),
+    new Process(5, 'Preto', 2, './assets/preto.png'),
 ];
 
 export const PROCESSES_T3: Process[] = [
-    new Process(6, 'ABC', 3, './assets/azul.png'),
-    new Process(7, '123', 3, './assets/azul.png'),
-    new Process(8, '!@#', 3, './assets/azul.png'),
-    new Process(9, '*()', 3, './assets/azul.png'),
+    new Process(6, 'Rosa', 3, './assets/rosa.png'),
+    new Process(7, 'Roxo', 3, './assets/roxo.png'),
+    new Process(8, 'Verde', 3, './assets/verde.png'),
+    new Process(9, 'Vermelho', 3, './assets/vermelho.png'),
 ];
