3dd215499369b181639f80c3e01395c70ce44970
HenriqueMaia <henrique.maia@poli.ufrj.br>
2018-08-05 11:00:21 -0300

    Tratamento de erro adicionado

--- src/app/services/simulator.service.ts
+++ src/app/services/simulator.service.ts
@@ -28,21 +28,9 @@ export class SimulatorService {
     var result = this.http.post<SimulationObject>(endpoint, simulationObject);
     result.subscribe(r => console.log(r));
     result.toPromise().catch(function(error) {
-      new Toast(error.message, Toast.TYPE_ERROR, Toast.TIME_LONG);
+      console.log(error);
+      new Toast(error.error.message, Toast.TYPE_ERROR, Toast.TIME_LONG);
     });
     return result;
-  }
-
-  private handleError<T> (operation = 'operation', result?: T) {
-    // console.log(result);
-    // new Toast("An error has occurred.", Toast.TYPE_ERROR, Toast.TIME_LONG);
-    return (error: any): Observable<T> => {
-
-      // TODO: send the error to remote logging infrastructure
-      console.error('aaaa'); // log to console instead
-      console.error(error); // log to console instead
-      // Let the app keep running by returning an empty result.
-      return of(result as T);
-    };
   }
 }
